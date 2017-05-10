18e738587aad7548c08056200a7386df0f3e2e7d
Saeid Dadkhah <saeid.dadkhah@live.com>
2017-05-10 21:14:16 +0430

    second is added

---
+++ Materials/second/xo.py
@@ -0,0 +1,19 @@
+def is_empty(x, y):
+    pass
+
+def put_char(x, y, c):
+    pass
+
+def game_over():
+    return 'A'
+
+# print('a')
+# x = input('Please enter x: ')
+# print(x)
+
+y = int(input('Please enter y: '))
+
+print(type(y))
+
+
+
